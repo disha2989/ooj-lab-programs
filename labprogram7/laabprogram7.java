4780cbf6e35fe72cd79208ab484aef6dc303c69d
disha2989 <71320045+disha2989@users.noreply.github.com>
2020-12-29 11:21:39 +0530

    Create laabprogram7.java

---
+++ labprogram7/laabprogram7.java
@@ -0,0 +1,50 @@
+class Generics<T, U,S> 
+{ 
+    T obj1; 
+    U obj2;
+    S obj3;  
+  
+	Generics(T obja, U objb,S objc) 
+	{
+		obj1=obja;
+		obj2=objb;
+		obj3=objc;
+	}
+	void showTypes()
+	{
+		System.out.println("Type of 1st parameter is " + obj1.getClass().getName());
+        System.out.println("Type of 2nd parameter is " + obj2.getClass().getName());
+		System.out.println("Type of 3rd parameter is " + obj3.getClass().getName());
+	}
+   
+	T getobj1()
+	{
+		return obj1;
+	}
+	U getobj2()
+	{
+		return obj2;
+	}
+	S getobj3()
+	{
+		return obj3;
+	}
+} 
+  
+class Genericsmain 
+{ 
+    public static void main (String[] args) 
+    { 
+	    String a,c;
+		int b;
+        Generics <String,Integer, String> obj = new Generics<String, Integer,String>("disha",2989,"1BM19CS051"); 
+		obj.showTypes();
+		
+        a=obj.getobj1();
+		b=obj.getobj2();
+		c=obj.getobj3();
+		
+		System.out.println( a +" "+ b +" "+ c );
+        //obj.print(); 
+    } 
+}
