70bce3abb5a1b7864263f0fb826760979dd38034
disha2989 <71320045+disha2989@users.noreply.github.com>
2020-12-29 10:51:28 +0530

    Create labprogram1.java

---
+++ labprogram1/labprogram1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class quadratic{
+	public static void main(String args[]){
+		Scanner s=new Scanner(System.in);
+		double a,b,c,d,r1,r2;
+		a =s.nextDouble();
+		b =s.nextDouble();
+		c =s.nextDouble();
+		d=(b*b)-(4*a*c);
+		if (d>0)
+		{
+			System.out.println("the quadratic equation has real and distinct roots");
+			r1=(-b+Math.sqrt(d))/(2*a);
+			r2=(-b-Math.sqrt(d))/(2*a);
+			System.out.println("roots are "+r1+" and "+r2);
+	    }else if(d==0){
+			System.out.println("the quadratic equation has real and equal roots");
+			r1=-b/(2*a);
+			r2=r1;
+			System.out.println("roots are "+r1+" and "+r2);
+		}else{
+			System.out.println("the quadratic equation has imaginary roots");
+		}
+	}
+}
+		
